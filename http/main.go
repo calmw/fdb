447d454fd27086ef9e0a07a629a7a76fbbee7d61
calmw <calm.wang@hotmail.com>
2024-02-11 13:24:18 +0800

    HTTP接口

---
+++ http/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/calmw/fdb"
+	"log"
+	"net/http"
+)
+
+var db *fdb.DB
+
+func init() {
+	// 初始化 DB 实例
+	var err error
+	options := fdb.DefaultOption
+	db, err = fdb.Open(options)
+	if err != nil {
+		panic(fmt.Sprintf("failed to open db: %v", err))
+	}
+}
+
+func main() {
+	// 注册处理方法
+	http.HandleFunc("/fdb/put", handlePut)
+	http.HandleFunc("/fdb/get", handleGet)
+	http.HandleFunc("/fdb/delete", handleDelete)
+	http.HandleFunc("/fdb/listkeys", handleListKeys)
+	http.HandleFunc("/fdb/stat", handleStat)
+
+	// 启动 HTTP 服务
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func handlePut(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodPost {
+		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var kv map[string]string
+
+	if err := json.NewDecoder(request.Body).Decode(&kv); err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for key, value := range kv {
+		if err := db.Put([]byte(key), []byte(value)); err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			log.Printf("failed to put kv in db: %v\n", err)
+			return
+		}
+	}
+}
+
+func handleGet(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodGet {
+		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	key := request.URL.Query().Get("key")
+
+	value, err := db.Get([]byte(key))
+	if err != nil && !errors.Is(err, fdb.ErrKeyNotFound) {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		log.Printf("failed to get kv in db: %v\n", err)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(writer).Encode(string(value))
+}
+
+func handleDelete(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodDelete {
+		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	key := request.URL.Query().Get("key")
+
+	err := db.Delete([]byte(key))
+	if err != nil && !errors.Is(err, fdb.ErrKeyIsEmpty) {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		log.Printf("failed to get kv in db: %v\n", err)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(writer).Encode("OK")
+}
+
+func handleListKeys(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodGet {
+		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	keys := db.ListKeys()
+	writer.Header().Set("Content-Type", "application/json")
+	var result []string
+	for _, k := range keys {
+		result = append(result, string(k))
+	}
+	_ = json.NewEncoder(writer).Encode(result)
+}
+
+func handleStat(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodGet {
+		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	stat := db.Stat()
+	writer.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(writer).Encode(stat)
+}
